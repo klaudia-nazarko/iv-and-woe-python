116d6ddf1dea14cd939d5b84a3dfcf69d1fedbee
Klaudia <klaudia.nazarko+data@gmail.com>
2020-01-08 20:17:11 +0100

    add continuous feature class

--- intro.py
+++ intro.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 
-df = pd.read_csv('data/telco_churn.csv')
+df = pd.read_csv('data/telco_churn.csv', na_values=[' '])
 df['label'] = df['Churn'].map({'Yes': 1, 'No': 0})
 
 
@@ -52,3 +52,65 @@ class Feature(object):
 feat_gender = Feature(df, 'gender')
 feat_contract = Feature(df, 'Contract')
 
+
+### Continuous variables
+# Group NaNs together
+# Split into X (eg 20 / 10) equal-size bins
+# Check if each bin is bigger than 5% of all observations
+# Non zero for events and non-events
+# Check if woe is monotonic
+# Iterate
+
+import scipy.stats.stats as stats
+
+class ContinuousFeature(Feature):
+    #label = 'label'
+
+    def __init__(self, df, feature):
+        super().__init__(df, feature)
+        self.bin_min_size = int(len(self.df_lite) * 0.05)
+
+    def generate_bins(self, bins_num):
+        df = self.df_lite
+        df['bin'] = pd.qcut(df[self.feature], bins_num, labels=False, duplicates='drop')
+        return df
+
+    def generate_correct_bins(self, bins_max):
+        for bins_num in range(bins_max, 1, -1):
+            with pd.option_context('mode.chained_assignment', None):
+                df = self.generate_bins(bins_num)
+            df_grouped = pd.DataFrame(df.groupby('bin') \
+                                      .agg({self.feature: 'count',
+                                            self.label: 'sum'})) \
+                                      .reset_index()
+            r, p = stats.spearmanr(df_grouped['bin'], df_grouped[self.label])
+
+            if (
+                    abs(r)==1 and       # check if woe for bins are monotonic
+                    df_grouped[self.feature].min() > self.bin_min_size      # check if bin size is greater than 5%
+                    and not (df_grouped[self.feature] == df_grouped[self.label]).any()      # check if number of good and bad is not equal to 0
+            ):
+                break
+
+        return df
+
+
+charges = ContinuousFeature(df, 'TotalCharges')
+x = charges.generate_correct_bins(20)
+
+tenure = ContinuousFeature(df, 'tenure')
+x = tenure.generate_correct_bins(20)
+
+
+########
+
+feature = 'tenure'
+bins_num = 20
+df[feature] = pd.to_numeric(df[feature])
+
+while bins_num > 0:
+    df['bin'] = pd.qcut(df[feature], bins_num, labels=False, duplicates='drop')
+    check = pd.DataFrame(df.groupby('bin').agg({feature: 'count', 'label': 'sum'})).reset_index()
+    r, p = stats.spearmanr(check['bin'], check['label'])
+    print(bins_num, r)
+    bins_num -= 1
